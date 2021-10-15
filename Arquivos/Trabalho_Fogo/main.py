021cce102ea18d03849d13dabfa992c073d76317
MateusFrancelino <32208103+MateusFrancelino@users.noreply.github.com>
2021-10-15 17:33:18 -0300

    Update main.py

--- Arquivos/Trabalho_Fogo/main.py
+++ Arquivos/Trabalho_Fogo/main.py
@@ -10,11 +10,9 @@ import os
 if __name__ == '__main__':
 
 
-    img_names = glob(os.path.join('fire_images','*.png'))
-    dir='./imagens_resultado'
-    os.mkdir(dir)
-    salvar=os.getcwd()+'/imagens_resultado'
-    i=0
+    img_names = glob(os.path.join('fire_images','*.png')) #<<< para pegar as imagens sem fogo
+    #img_names = glob(os.path.join('non_fire_images','*.png')) #<<< para pegar as imagens com fogo
+
 
     for fn in img_names:
         image=cv2.imread(fn)
@@ -27,27 +25,13 @@ if __name__ == '__main__':
         contour = fire_detector.apply_fire_contour(mask)
         contoured_image = fire_detector.add_contour2image(contour)
 
-        # cv2.imshow('Original', image)
-        # cv2.imshow('Fire', fire)
-        # cv2.imshow('Non Fire', non_fire)
-        # cv2.imshow('Mask', mask)
-        # cv2.imshow('Canny', canny)
-        # cv2.imshow('Contour', contour)
-        # cv2.imshow('Contoured Image', contoured_image)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemfogo.jpg' %i),image)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemfogo.jpg' %i),fire)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemsemfogo.jpg' %i),non_fire)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemMask.jpg' %i),mask)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemcanny.jpg' %i),canny)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemcontour.jpg' %i),contour)
-        cv2.imwrite(os.path.join(salvar,'%04iImagemcontornada.jpg' %i),contoured_image)
-
-        i+=1
-
-
-
-
-
+        cv2.imshow('Original', image)
+        cv2.imshow('Fire', fire)
+        cv2.imshow('Non Fire', non_fire)
+        cv2.imshow('Mask', mask)
+        cv2.imshow('Canny', canny)
+        cv2.imshow('Contour', contour)
+        cv2.imshow('Contoured Image', contoured_image)
 
 
 
